dccba1f19b0c58f2ae89978dcff1ad21c3b24894
Erez Rabih <erez.rabih@gmail.com>
2013-11-25 12:00:28 +0200

    removed puts from app loader

--- load_app.rb
+++ load_app.rb
@@ -2,5 +2,5 @@ ROOT_PATH = File.expand_path("../", __FILE__)
 $LOAD_PATH.unshift("#{ROOT_PATH}/lib", "#{ROOT_PATH}/app")
 
 %w{config/initializers lib app}.each do |dir|
-  Dir["#{File.dirname(__FILE__)}/#{dir}/**/*.rb"].each { |f| puts f ;require(f) }
+  Dir["#{File.dirname(__FILE__)}/#{dir}/**/*.rb"].each { |f| require(f) }
 end
