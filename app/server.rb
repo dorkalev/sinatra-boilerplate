9cef42310c64686fcd22ce92010a83724d4050d2
Dor Kalev <dor@dorkalev.com>
2013-11-28 20:42:57 +0200

    use class container for the server

--- app/server.rb
+++ app/server.rb
@@ -1,10 +1,13 @@
 require "sinatra/reloader" if development?
 
-helpers ExampleHelpers
 
-get "/" do
-  env_config = CONFIG['key']
-  helper = helper_method
+class SinatraApp < Sinatra::Base
+  helpers ExampleHelpers
 
-  "Hello World! #{env_config} #{helper}"
+  get "/" do
+    env_config = CONFIG['key']
+    helper = helper_method
+
+    "Hello World! #{env_config} #{helper}"
+  end
 end
